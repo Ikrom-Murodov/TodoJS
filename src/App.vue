de634be50ae13f58b390e2e900ab9284f47b5787
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-02-28 15:51:25 +0500

    Удалил не нужные компоненты и настроил проект для работы

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="content">
+    <div class="content__container">
+      <div class="content__todo"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  methods: {},
+  watch: {}
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import "./style/base"
 </style>
